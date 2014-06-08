842e2f4d85a12ab4a62325f993904075a9c426ed
Rohan Prinja <rohan.prinja@gmail.com>
2014-06-09 02:35:12 +0530

    update todo, implement friction and spotlight following

--- js/game.js
+++ js/game.js
@@ -3,43 +3,48 @@
 
 var renderer, scene, camera, spotLight;
 var spotLights = new Array(8);
+var groundRadius = 250;
 
 var fieldWidth = 400, fieldHeight = 200;
 
 var ball;
-var score;
+// var score;
+var groundFriction = 0.7;
 
 function setup() {
-	score = 0;
+	// score = 0;
 	createScene();
 	draw();
 }
 
 function createScene() {
-	var WIDTH = 640, HEIGHT = 480;
-	var VIEW_ANGLE = 50, ASPECT = WIDTH / HEIGHT, NEAR = 0.1, FAR = 10000;
+	var WIDTH = 640, 
+        HEIGHT = 480;
+	var VIEW_ANGLE = 50,
+        ASPECT = WIDTH / HEIGHT,
+        NEAR = 0.1,
+        FAR = 10000;
 
+    /* set up renderer and attach it to the relevant DOM element */
 	var container = document.getElementById("gameCanvas");
-
 	renderer = new THREE.WebGLRenderer();
+    renderer.setSize(WIDTH, HEIGHT);
+    container.appendChild(renderer.domElement);
+
+    /* initialise a new scene */
     scene = new THREE.Scene();
 
-	/* camera */
+    /* camera */
     camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
     camera.position.z = 200;
     camera.position.y = -400;
     camera.rotation.x = 1.1;
-	scene.add(camera);
-
-    renderer.setSize(WIDTH, HEIGHT);
-
-    container.appendChild(renderer.domElement);
+    scene.add(camera);
 
     /* marble ball */
     var ballRadius = 10, segments = 32, rings = 32;
     var ballGeometry = new THREE.SphereGeometry(ballRadius, segments, rings);
-    // var ballMaterial = new THREE.MeshPhongMaterial({  color: 0xff0000, transparent: true, opacity: 0.8 });
-    var ballMaterial = new THREE.MeshPhongMaterial({  color: 0xff0000 });
+    var ballMaterial = new THREE.MeshPhongMaterial({  color: 0xff0000 /*, transparent: true, opacity: 0.8*/ });
 
     ball = new THREE.Mesh(ballGeometry, ballMaterial);
     ball.position.z = ballRadius;
@@ -53,29 +58,28 @@ function createScene() {
     scene.add(ball);
 
     spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(0, 0, 170);
-    spotLight.intensity = .6;
+    spotLight.position.set(0, 0, 210);
+    spotLight.intensity = 1;
     spotLight.angle = Math.PI / 2;
     spotLight.castShadow = true;
     spotLight.shadowCameraVisible = false;
-    spotLight.shadowDarkness = 0.1;
+    spotLight.shadowDarkness = 0.6;
     scene.add(spotLight);
 
-    scene.add(new THREE.AmbientLight(0x101010));
+    /* eight spotlights placed at equal distances along the perimeter */
+    // scene.add(new THREE.AmbientLight(0x101010));
 
-    // var spotLights = new Array(8);
-    for (var i = 0; i <= 8; i++) {
-        spotLights[i] = new THREE.SpotLight(0xF7F5BC);
-        spotLights[i].intensity = .2;
-        // spotLights[i].distance = 100;
-        spotLights[i].position.set(200 * Math.sin(i * Math.PI / 4),
-                                200 * Math.cos(i * Math.PI / 4),
-                                100);
-        spotLights[i].castShadow = true;
-        spotLights[i].shadowDarkness = 0.1;
+    // for (var i = 0; i <= 8; i++) {
+    //     spotLights[i] = new THREE.SpotLight(0xF7F5BC);
+    //     spotLights[i].intensity = .2;
+    //     spotLights[i].position.set(groundRadius * Math.sin(i * Math.PI / 4),
+    //                             groundRadius * Math.cos(i * Math.PI / 4),
+    //                             100);
+    //     spotLights[i].castShadow = true;
+    //     spotLights[i].shadowDarkness = 0.1;
 
-        scene.add(spotLights[i]);
-    };
+    //     scene.add(spotLights[i]);
+    // };
 
 
     renderer.shadowMapEnabled = true;
@@ -83,21 +87,10 @@ function createScene() {
 
     /* plane */
     var planeMaterial = new THREE.MeshLambertMaterial({ color: 0x51A8F5 });
-
-    // var planeWidth = fieldWidth, planeHeight = fieldHeight, planeQuality = 10;
-    // var plane = new THREE.Mesh(
-    //  new THREE.PlaneGeometry(planeWidth,
-    //                          planeHeight,
-    //                          planeQuality,
-    //                          planeQuality),
-    //  planeMaterial);
-    var plane = new THREE.Mesh(new THREE.CircleGeometry(200, 32), planeMaterial);
+    var plane = new THREE.Mesh(new THREE.CircleGeometry(groundRadius, 32), planeMaterial);
 
     scene.add(plane);
     plane.receiveShadow = true;
-    // console.log(ball.position);
-    // console.log(plane.position);
-    // console.log(spotLights[1].position);
 }
 
 function draw() {
@@ -105,33 +98,44 @@ function draw() {
     requestAnimationFrame(draw);
 
     ballPhysics();
+    cameraPhysics();
     ballMovement();
 }
 
 function ballPhysics() {
     var dt = 0.25;
-    // ball.position.x += ball.velocity.x*dt + 0.5*ball.acceleration.x*dt*dt;
-    // ball.position.y += ball.velocity.y*dt + 0.5*ball.acceleration.y*dt*dt;
 
+    /* apply friction */
+    var x_acc_direction = Math.sign(ball.acceleration.x);
+    var y_acc_direction = Math.sign(ball.acceleration.y);
+
+    var x_friction = ball.velocity.x != 0 ? groundFriction : 0;
+    var y_friction = ball.velocity.y != 0 ? groundFriction : 0;
+
+    ball.acceleration.x -= (x_acc_direction * x_friction);
+    ball.acceleration.y -= (y_acc_direction * y_friction);
+
+    if (x_acc_direction * ball.acceleration.x < 0) { ball.acceleration.x = 0; }
+    if (y_acc_direction * ball.acceleration.y < 0) { ball.acceleration.y = 0; }
+
+    /* new velocities */
     ball.velocity.x += ball.acceleration.x * dt;
+    ball.velocity.y += ball.acceleration.y * dt;
+
+    /* clamp velocities */
     if (ball.velocity.x > ball.maxVelocity) { ball.velocity.x = ball.maxVelocity; }
     if (ball.velocity.x < -ball.maxVelocity) { ball.velocity.x = -ball.maxVelocity; }
-    ball.velocity.y += ball.acceleration.y * dt;
     if (ball.velocity.y > ball.maxVelocity) { ball.velocity.y = ball.maxVelocity; }
     if (ball.velocity.y < -ball.maxVelocity) { ball.velocity.y = -ball.maxVelocity; }
 
+    /* new positions */
     ball.position.x += ball.velocity.x * dt;
     ball.position.y += ball.velocity.y * dt;
 
-    var xx = ball.position.x;
-    var yy = ball.position.y;
+    // var xx = ball.position.x;
+    // var yy = ball.position.y;
 
     // if (xx*xx + yy*yy >= 40000) { ball.acceleration.x =  }
-
-    console.log(ball.position);
-    console.log(ball.velocity);
-    console.log(ball.acceleration);
-    console.log("...");
 }
 
 function ballMovement() {
@@ -143,5 +147,22 @@ function ballMovement() {
         ball.acceleration.x = -ball.maxAcceleration;
     } else if (Key.isDown(Key.D)) {
         ball.acceleration.x = ball.maxAcceleration;
+    } else if (Key.isDown(Key.Q)) {
+        ball.velocity = { x: 0, y: 0 };
+        ball.acceleration = { x: 0, y: 0 };
+    } else {
+        /* no acceleration unless a key is pressed */
+        ball.acceleration = { x: 0, y: 0 };
     }
+}
+
+function cameraPhysics() {
+    // spotLight.position.x = ball.position.x;
+    // spotLight.position.y = ball.position.y;
+
+    spotLight.target.position.x = ball.position.x;
+    spotLight.target.position.y = ball.position.y;
+
+    camera.position.x = 0.5 * ball.position.x;
+    camera.position.y = -400 + 0.5 * ball.position.y;
 }
